d000446747d7e3008e342b683174b6dee2984424
michelzarpe <michelzarpe@gmail.com>
2025-05-08 10:58:19 -0300

    adicionando comentarios para entender o que faz

--- rest/src/main/kotlin/santannaf/customer/rest/Application.kt
+++ rest/src/main/kotlin/santannaf/customer/rest/Application.kt
@@ -4,10 +4,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import reactor.core.publisher.Hooks
 
+/*
+O atributo `scanBasePackages` na anotação `@SpringBootApplication` está sendo usado para especificar explicitamente
+os pacotes que o Spring deve escanear em busca de componentes, serviços, repositórios, etc., durante a inicialização da aplicação.
+No seu caso, o valor `["santannaf"]` indica que o Spring deve escanear todos os subpacotes dentro de `santannaf`. Isso é útil quando os componentes da aplicação estão organizados em
+pacotes fora do pacote padrão onde a classe principal (`Application`) está localizada. Sem isso, o Spring não encontraria automaticamente os componentes definidos em outros pacotes.
+ */
 @SpringBootApplication(scanBasePackages = ["santannaf"])
 class Application
 
 fun main(args: Array<String>) {
+    /*
+    A linha `Hooks.enableAutomaticContextPropagation();` ativa a propagação automática de contexto no Reactor,
+    que é uma biblioteca para programação reativa. Essa funcionalidade permite que o contexto (como informações
+    de rastreamento ou segurança) seja automaticamente propagado entre os fluxos reativos, sem a necessidade
+    de gerenciá-lo manualmente. Isso é útil em aplicações Spring Boot que utilizam programação reativa, garantindo
+     que informações contextuais sejam mantidas ao longo do processamento assíncrono.
+
+     */
     Hooks.enableAutomaticContextPropagation();
     runApplication<Application>(args = args)
 }
--- rest/src/main/kotlin/santannaf/customer/rest/entrypoint/PeopleController.kt
+++ rest/src/main/kotlin/santannaf/customer/rest/entrypoint/PeopleController.kt
@@ -31,20 +31,26 @@ class PeopleController(
     @ResponseStatus(HttpStatus.CREATED)
     fun createPeople(
         @RequestBody request: PeopleRequest
-    ): Mono<ResponseEntity<*>> {
-        return Mono.fromCallable { request }
-            .subscribeOn(Schedulers.boundedElastic())
-            .flatMap(saveUseCase::savePeople)
-            .timeout(Duration.ofMillis(TIMEOUT))
-            .map {
-                ResponseEntity.created(
-                    UriComponentsBuilder
-                        .fromPath("/pessoas/{id}")
-                        .buildAndExpand(it)
-                        .toUri()
-                ).build<Void>()
+    ): Mono<ResponseEntity<*>> { // Define o retorno do método como um `Mono` que encapsula um `ResponseEntity` genérico.
+        /*
+        A linha selecionada cria um `Mono` a partir de uma chamada que encapsula o objeto `request`.
+        O metodo `Mono.fromCallable` é usado para executar uma operação de forma assíncrona e não-bloqueante. Nesse caso, ele encapsula o objeto `request` dentro de um fluxo reativo (`Mono`),
+         permitindo que ele seja processado posteriormente no pipeline reativo.
+        */
+        return Mono.fromCallable { request } // Cria um `Mono` a partir de uma chamada que encapsula o objeto `request`.
+            .subscribeOn(Schedulers.boundedElastic()) // Define que a execução será feita em uma thread do pool `boundedElastic`.
+            .flatMap(saveUseCase::savePeople) // Encadeia a chamada ao método `savePeople` do `SavePeopleUseCase`, que retorna um `Mono<UUID>`.
+            .timeout(Duration.ofMillis(TIMEOUT)) // Define um tempo limite para a execução do fluxo reativo.
+            .map { // Transforma o resultado (UUID) em uma resposta HTTP.
+                ResponseEntity.created( // Cria uma resposta HTTP com status `201 Created`.
+                    UriComponentsBuilder // Constrói a URI do recurso criado.
+                        .fromPath("/pessoas/{id}") // Define o caminho base da URI com um placeholder para o ID.
+                        .buildAndExpand(it) // Substitui o placeholder `{id}` pelo valor do UUID retornado.
+                        .toUri() // Converte a URI construída para o formato `java.net.URI`.
+                ).build<Void>() // Finaliza a construção do `ResponseEntity` sem corpo.
             }
     }
+
 
     @GetMapping(path = ["/pessoas/{id}"])
     fun fetchPeopleById(@PathVariable id: UUID): Mono<ResponseEntity<*>> {
